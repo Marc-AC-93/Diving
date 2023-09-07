efc6f3dcd24b8af070ef7fbabbba0a411f2a9fe6
Marc_AC <marc.aroca@avios.com>
2023-09-07 16:02:36 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Diving
-Calculadora de consumo de aire
+
+## Objetivo
+
+Crear una calculadora de consumo de aire para las inmersiones del buceo.
+
+## Formula
+
+![formula.png](formula.png)
+
+## Setup
+
+1. Instalar [python3](https://www.python.org/downloads/)
+2. Clonar el repositorio 
+   - Abrir la terminal
+   - Ejecutar el siguiente comando: `git clone git@github.com:Marc-AC-93/Diving.git`
+
+## Calcular el consumo de aire
+
+1. Abrir la terminal (en la carpeta del proyecto)
+2. ejecutar el script: `python3 calculadora.py`
+3. Ir introduciendo los datos qué pide la calculadora para resolver la formula.
+
+Al acabar de introducir los datos te devolverá el consumo de aire basado en:
+ - bares de la botella
+ - litros de la botella
+ - produndidad media
+ - tiempo de buceo
+
+Si el resultado es un buen ratio (0.1 - 20 bares/minutos) se devolverá en color verde, sino en rojo.
---
+++ calculadora.py
@@ -0,0 +1,44 @@
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+try:
+    bar_ini = int(input("Bares iniciales: "))
+except:
+    print("[Error] Los bares deben ser un número")
+    exit()
+
+try:
+    bar_fin = int(input("Bares finales: "))
+except:
+    print("[Error] Los bares deben ser un número")
+    exit()
+
+try:
+    tankL = int(input("Litros del tanque: "))
+except:
+    print("[Error] Los litros del tanque deben ser un número")
+    exit()
+
+try:
+    divingTime = int(input("Tiempo de inmersión (minutos): "))
+except:
+    print("[Error] El tiempo total debe ser un número")
+    exit()
+
+try:
+    divingDeepAvg = (float(input("Profundidad media de la inmersión (metros): ")))/ 10 + 1
+except:
+    print("[Error] La profundidad media debe ser un número decimal expresado con punto (no usar coma)")
+    exit()
+
+total= str(round(float((((bar_ini - bar_fin) * tankL) / divingDeepAvg ) / divingTime), 2))
+
+color = bcolors.GREEN if 20 > float(total) > 0 else bcolors.RED
+print(f"\n{bcolors.BOLD}RESULTADO{bcolors.ENDC}\n{bcolors.BOLD}Consumo ={bcolors.ENDC} {color}{total}{bcolors.ENDC} bares / minuto")
